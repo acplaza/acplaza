abd004c7794edf283cfcd019a7a9f34cf3a18abc
io <io@mintz.cc>
2020-06-16 22:02:26 +0000

    frontend: add design listing endpoints

--- views/frontend.py
+++ views/frontend.py
@@ -7,9 +7,10 @@ from flask import Blueprint, render_template, session, request, redirect, url_fo
 
 import utils
 from views import api
-from acnh.common import ACNHError
+from acnh.common import ACNHError, acnh
 from acnh.designs import api as designs_api
 from acnh.designs import render as designs_render
+from acnh.designs import encode as designs_encode
 
 def init_app(app):
 	app.register_blueprint(bp)
@@ -44,30 +45,79 @@ def design_form():
 	except KeyError:
 		return render_template('design_form.html', design_code_regex=designs_api.InvalidDesignCodeError.regex.pattern)
 
+@bp.route('/designs/')
+def designs_form():
+	try:
+		return redirect(url_for('.basic_designs', author_id=request.args['author_id']))
+	except KeyError:
+		return render_template('designs_form.html', author_id_regex=designs_api.InvalidAuthorIdError.regex.pattern)
+
 bp.route('/design/<design_code>/<layer>.png')(api.design_layer)
+bp.route('/design/<design_code>.tar')(api.design_archive)
 
 @bp.route('/design/<design_code>')
 def design(design_code):
-	data = designs_api.download_design(designs_api.design_id(design_code))
-	meta, body = data['mMeta'], data['mData']
-	type_code = meta['mMtUse']
+	data = designs_api.download_design(design_code)
+	meta = data['mMeta']
 	design_name = meta['mMtDNm']
 
-	layers = designs_render.render_external_layers(body, type_code)
+	design = designs_encode.Design.from_data(data)
+
 	images = (
 		(name.capitalize().replace('-', ' '), utils.image_to_base64_url(utils.xbrz_scale_wand_in_subprocess(image, 6)))
 		for name, image
-		in designs_render.render_external_layers(body, type_code).items()
+		in design.layer_images.items()
 	)
 
 	return render_template(
 		'design.html',
 		created_at=dt.datetime.utcfromtimestamp(data['created_at']),
 		author_name=data['author_name'],
-		author_id=data['author_id'],
+		author_id=designs_api.add_hyphens(str(data['author_id'])),
+		design_code=design_code,
 		design_name=design_name,
+		design_type=type(design).display_name,
 		island_name=meta['mMtVNm'],
 		layers=images,
+	)
+
+@bp.route('/designs/<author_id>')
+def basic_designs(author_id):
+	return designs(author_id, pro=False)
+
+@bp.route('/pro-designs/<author_id>')
+def pro_designs(author_id):
+	return designs(author_id, pro=True)
+
+def designs(author_id, *, pro):
+	author_id = int(designs_api.InvalidAuthorIdError.validate(author_id).replace('-', ''))
+	pretty_author_id = designs_api.add_hyphens(str(author_id))
+	data = designs_api.list_designs(author_id, pro=pro)
+	if not data['total']:
+		return render_template(
+			'no_designs.html',
+			pro=pro, design_type='Pro' if pro else 'basic', author_id=pretty_author_id,
+		)
+
+	author_name = data['headers'][0]['design_player_name']
+
+	designs = []
+	for header in data['headers']:
+		data = designs_api.download_design(header['id'])
+		layer_0 = designs_render.render_layer(data['mData'], 0)
+		designs.append((
+			data['mMeta']['mMtDNm'],
+			designs_api.design_code(header['id']),
+			url_for('.design_layer', design_code=designs_api.design_code(header['id']), layer='thumbnail'),
+		))
+
+	return render_template(
+		'designs.html',
+		author_id=pretty_author_id,
+		author_name=author_name,
+		pro=pro,
+		designs=designs,
+		design_type='Pro' if pro else 'basic',
 	)
 
 @bp.errorhandler(ACNHError)
