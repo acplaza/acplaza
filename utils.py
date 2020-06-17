acplaza/acplaza

commit c7c02751f4757bdcccd6f0a8c32b4c97223ff443

fix utils.process_authorization

utils.py
@@ -96,7 +96,8 @@ def process_authorization():
 	if not token:
 		raise IncorrectAuthorizationError
 
-	if not validate_token(token):
+	user_id, secret = validate_token(token)
+	if not user_id:
 		raise IncorrectAuthorizationError
 
 	request.user_id = user_id
@@ -109,12 +110,12 @@ def validate_token(token):
 
 	db_secret = pg().fetchval(queries.secret(), user_id)
 	if db_secret is None:
-		return False
+		return False, False
 
 	if not secrets.compare_digest(secret, db_secret):
-		return False
+		return False, False
 
-	return True
+	return user_id, secret
 
 def encode_token(user_id, secret):
 	left = str(user_id)

commit a8167aa50d6bdafc9057f2ee0b4ea307018b594b

fix some critical security bugs
due to a regression in c7c02751f4757bdcccd6f0a8c32b4c97223ff443, anyone could log in with any valid token!

utils.py
@@ -84,9 +84,6 @@ def token_exempt(view):
 def process_authorization():
 	request.user_id = None
 
-	if get_ipaddr() == '127.0.0.1':
-		return
-
 	if not request.endpoint:
 		return
 
@@ -98,7 +95,7 @@ def process_authorization():
 	if not request.headers.get('User-Agent'):
 		raise MissingUserAgentStringError
 
-	if session.get('authed'):
+	if session.get('user_id'):
 		return
 
 	token = request.headers.get('Authorization')
@@ -115,7 +112,7 @@ def validate_token(token):
 	try:
 		user_id, secret = parse_token(token)
 	except ValueError:
-		return False
+		return False, False
 
 	db_secret = pg().fetchval(queries.secret(), user_id)
 	if db_secret is None:

views/frontend.py
@@ -41,16 +41,17 @@ def login():
 		# with the form
 		abort(HTTPStatus.UNAUTHORIZED)
 
-	if not utils.validate_token(token):
+	user_id, secret = utils.validate_token(token)
+	if not user_id:
 		return 'auth failed', HTTPStatus.UNAUTHORIZED
 
-	session['authed'] = 1
+	session['user_id'] = user_id
 	return redirect('/')
 
 @bp.route('/logout')
 @utils.token_exempt
 def logout():
-	session.pop('authed', None)
+	session.clear()
 	return redirect('/')
 
 @bp.route('/')